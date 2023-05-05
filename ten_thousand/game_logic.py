1f02f98ddaa7b55c45a316849eae66efb2a70c53
DohaKhamaiseh <dohakhamaiseh1999@gmail.com>
2023-05-05 21:06:21 +0000

    lab07 updated

--- ten_thousand/game_logic.py
+++ ten_thousand/game_logic.py
@@ -5,117 +5,108 @@ import random
 class GameLogic :
   
   @staticmethod
-  def calculate_score(t):
-   result = 0
-   d_counter = Counter(t)
+  def calculate_score(dice):
+        """
+        this function will calculate the score for dice or multiple dice depending on ten thousand's game rules
+        """
+        score = 0
 
-   if(d_counter[5]==1):
-     result +=  50
+        dice_set = set(dice)
+        pairs = []
 
-   if (d_counter[1]==1):
-     result += 100
+        if len(dice) == 0:
+            return 0
+        
+        if dice_set == {1,2,3,4,5,6}:
+            score += 1500
+            return score
 
-   if(d_counter[5]==2):
-      result +=  100
+        for i in dice_set:
+            num_of_kinds = dice.count(i)
+            if num_of_kinds == 2:
+                pairs.append(2)
+            if i == 1:
+                if num_of_kinds == 1:
+                    score += 100
+                elif num_of_kinds == 2:
+                    score += 200
+                elif num_of_kinds == 3:
+                    score += 1000
+                elif num_of_kinds == 4:
+                    score += 2000
+                elif num_of_kinds == 5:
+                    score += 4000
+                elif num_of_kinds == 6:
+                    score += 8000
+                
+            
+            elif i == 2:
+                if num_of_kinds == 3:
+                    score += 200
+                elif num_of_kinds == 4:
+                    score += 400
+                elif num_of_kinds == 5:
+                    score += 800
+                elif num_of_kinds == 6:
+                    score += 1600
+                    
+            elif i == 3:
+                if num_of_kinds == 3:
+                    score += 300
+                elif num_of_kinds == 4:
+                    score += 600
+                elif num_of_kinds == 5:
+                    score += 1200
+                elif num_of_kinds == 6:
+                    score += 2400
 
-   if(d_counter[1]==2):
-      result +=  200
+            elif i == 4:
+                if num_of_kinds == 3:
+                    score += 400
+                elif num_of_kinds == 4:
+                    score += 800
+                elif num_of_kinds == 5:
+                    score += 1600
+                elif num_of_kinds == 6:
+                    score += 3200
+        
+            elif i == 5:
+                if num_of_kinds == 1:
+                    score += 50
+                elif num_of_kinds == 2:
+                    score += 100   
+                elif num_of_kinds == 3:
+                    score += 500
+                elif num_of_kinds == 4:
+                    score += 1000
+                elif num_of_kinds == 5:
+                    score += 2000
+                elif num_of_kinds == 6:
+                    score += 4000
 
-   if(d_counter[2]==1 or d_counter[2]==2 ):
-      result +=  0
-    
-   if(d_counter[5]==3):
-      result +=  500
-    
-   if(d_counter[1]==3):
-      result +=  1000
+            elif i == 6:
+                if num_of_kinds == 3:
+                    score += 600
+                elif num_of_kinds == 4:
+                    score += 1200
+                elif num_of_kinds == 5:
+                    score += 2400
+                elif num_of_kinds == 6:
+                    score += 4800
+
    
-   if( d_counter[1]==1 and d_counter[2]==1 and d_counter[3]==1 and d_counter[4]==1 and d_counter[5]==1 and d_counter[6]==1):
-     result =  1500
-    
-   if(d_counter[2]==3):
-     result += 200
-    
-   if(d_counter[2]==4):
-     result += 400
 
-   if(d_counter[2]==5):
-     result += 800
-
-   if(d_counter[2]==6):
-     result = 1600
-
-   if(d_counter[1]==6):
-     result = 8000
-    
-   if(d_counter[1]==4):
-     result = 2000
-    
-   if(d_counter[1]==5):
-     result += 4000
-
-   if(d_counter[3]==1 or d_counter[3]==2):
-     result += 0 
-
-   if(d_counter[3]==3):
-     result += 300
-
-   if(d_counter[3]==4):
-     result += 600
-
-   if(d_counter[3]==5):
-     result += 1200
-
-   if(d_counter[3]==6):
-     result = 2400
-
-   if(d_counter[4]==1 or d_counter[4]==2):
-     result += 0 
-    
-   if(d_counter[4]==3):
-     result += 400 
-    
-   if(d_counter[4]==4):
-     result += 800 
-
-   if(d_counter[4]==5):
-     result += 1600 
-
-   if(d_counter[4]==6):
-     result += 3200 
-    
-   if(d_counter[5]==4):
-     result += 1000 
-
-   if(d_counter[5]==5):
-     result += 2000 
-
-   if(d_counter[5]==6):
-     result += 4000 
-    
-   if(d_counter[6]==1 or d_counter[6]==2):
-     result += 0 
-    
-   if(d_counter[6]==3):
-     result += 600 
-    
-   if(d_counter[6]==4):
-     result += 1200 
-
-   if(d_counter[6]==5):
-     result += 2400 
-
-   if(d_counter[6]==6):
-     result += 4800 
-    
-   if(d_counter[2]==2 and d_counter[3]==2 and d_counter[6]==2 ):
-     result =1500
-
-   return result
+            
+        if len(pairs) == 3:
+            score = 1500
+        return score
  
    
   @staticmethod
   def roll_dice(dice_num=6):
+       """ 
+       this function will return random numbers between 1 and 6 which represent the num on the dice
+       """
        random_numbers = []
 
        for i in range(dice_num):
